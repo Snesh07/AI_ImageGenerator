65254b654fc70c1612047ffdc3e60ba61cdf2fee
Sneha Sharma <87299768+SynonymOfLove@users.noreply.github.com>
2023-08-24 22:30:54 +0530

    mongoDB setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from "./mongodb/connect.js";
+
 
 dotenv.config();
 
@@ -12,3 +14,19 @@ app.use(express.json({ limit: '50mb'}));
 app.get('/', async (req, res) => {
     res.send('Hello from Dall-E!');
 })
+
+
+const startServer = async () => {
+
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+    } catch (error) {
+        console.log(error);
+    }
+
+
+    
+}
+
+startServer();
