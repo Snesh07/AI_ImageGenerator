b7c113b05912dd3a6c028be5da7ae3eff639fd11
Sneha Sharma <87299768+SynonymOfLove@users.noreply.github.com>
2023-09-10 21:49:04 +0530

    feat: cloudinary added

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,16 +17,39 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setloading] = useState(false);
 
-  const generateImage = () =>{
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        })
 
+        const data = await response.json();
+
+        setForm({ ...form, photos: `data:image/jpeg;base64,${data.photo}` })
+
+      } catch (error) {
+        alert(error);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please enter a prompt')
+    }
   }
-  
+
+
   const handleSubmit = () => {
 
   };
 
   const handleChange = (e) => {
-    setForm({ ...form, [e.target.name] : e.target.value })
+    setForm({ ...form, [e.target.name]: e.target.value })
   }
 
   const handleSurpriseMe = () => {
@@ -34,7 +57,7 @@ const CreatePost = () => {
     setForm({ ...form, prompt: randomPrompt })
   }
 
- 
+
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -77,7 +100,7 @@ const CreatePost = () => {
                 className="w-full h-full object-contain"
               />
             ) : (
-              <img 
+              <img
                 src={preview}
                 alt="preview"
                 className="w-9/12 h-9/12 boject-contain opacity-40"
@@ -88,16 +111,16 @@ const CreatePost = () => {
               <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg]">
                 <Loader />
               </div>
-      
+
             )}
           </div>
         </div>
 
         <div className="mt-5 flex gap-5">
           <button
-          type="button"
-          onClick={generateImage}
-          className="w-full text-white bg-green-700 font-medium rounded-md
+            type="button"
+            onClick={generateImage}
+            className="w-full text-white bg-green-700 font-medium rounded-md
           text-sm px-5 py-2.5 sm:w-auto text-center"
           >
             {generatingImg ? 'Generating...' : 'Generate'}
@@ -105,18 +128,19 @@ const CreatePost = () => {
         </div>
 
         <div className="mt-10">
-        <p className="mt-2 text-[#666e75] text-[14px]">
-        Once you have created the image you want, you can share it with others in the community.</p>
-        <button
-        type="submit"
-        className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-        >
-       {loading ? 'Sharing...' : 'Share with the community'}
-        </button>
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            Once you have created the image you want, you can share it with others in the community.</p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing...' : 'Share with the community'}
+          </button>
         </div>
       </form>
     </section>
   );
 };
+
 
 export default CreatePost;
