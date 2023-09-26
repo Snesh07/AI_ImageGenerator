b1318dc2cbfca3c73613cc0ae12fba4bb787f7c7
Sneha Sharma <87299768+SynonymOfLove@users.noreply.github.com>
2023-09-26 23:40:32 +0530

    button created

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -13,7 +13,7 @@ const Card = ({ _id, name, prompt, photo }) => {
       />
       <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0
   left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
-        <p className="text-white text-sm overflow-y-auto prompt">{prompt}</p>
+        <p className="text-white text-md overflow-y-auto prompt">{prompt}</p>
         <div className="mt-5 flex justify-between items-center gap-2">
           <div className="flex items-center gap-2">
             <div className="w-7 h-7 rounded-full object-cover bg-green-700
@@ -22,7 +22,12 @@ const Card = ({ _id, name, prompt, photo }) => {
             </div>
             <p className="text-white text-sm">{name}</p>
           </div>
-
+          <button type="button" onClick={() => downloadImage(_id, photo)}
+            className="outlive-none bg-transparent border-none"
+          >
+            <img src={download} alt="download"
+              className="w-6 h-6 object-contain invert" />
+          </button>
         </div>
       </div>
     </div>
